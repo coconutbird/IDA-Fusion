cf76d6b9a3285a8b18cb65c385093f2eced5b080
Jon-Luke Biddle <jonluke.biddle@gmail.com>
2025-12-15 16:22:15 -0700

    toolchains

---
+++ cmake/toolchain-x86.cmake
@@ -0,0 +1,14 @@
+﻿# Toolchain file for Windows x86 (32-bit)
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR X86)
+
+# Force 32-bit build
+set(CMAKE_SIZEOF_VOID_P 4)
+
+# Set architecture-specific flags
+if(MSVC)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /arch:SSE2" CACHE STRING "" FORCE)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /arch:SSE2" CACHE STRING "" FORCE)
+endif()
+
